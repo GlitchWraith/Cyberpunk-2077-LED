b87c182fefeae087fd69f6e6e1d35615dc2965f1
Tibbles <5703945+RealmOfTibbles@users.noreply.github.com>
2019-05-17 21:07:34 +0100

    MoreTesting

--- Base.py
+++ Base.py
@@ -3,24 +3,29 @@ import random
 import board
 import adafruit_dotstar as dotstar
 
-# On-board DotStar for boards including Gemma, Trinket, and ItsyBitsy
-# dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 2, brightness=0.2)
-
+dots_len = 30
+dots_lum = 0.3
 # Using a DotStar Digital LED Strip with 30 LEDs connected to hardware SPI
-dots = dotstar.DotStar(board.SCK, board.MOSI, 30, brightness=0.3)
+dots = dotstar.DotStar(board.SCK, board.MOSI, dots_len, brightness=dots_lum)
 
-# Using a DotStar Digital LED Strip with 30 LEDs connected to digital pins
-# dots = dotstar.DotStar(board.D6, board.D5, 30, brightness=0.2)
 
+# Left side
+bank_A_Start = 0
+bank_A_End = 15
+# Right side
+bank_B_start = 16
+bank_B_End = 30
 
 # HELPERS
 # a random color 0 -> 224
 def random_color():
     return random.randrange(0, 7) * 32
 
+def random_color_limited():
+    return random.randrange(1, 14) * 15
 
 # MAIN LOOP
-n_dots = len(dots)
+n_dots = dots_len
 while True:
     # Fill each dot with a random color
     for dot in range(n_dots):
