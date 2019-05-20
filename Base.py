566e9ca0a7aabbb55b81e6d812c0949c878e0c9a
Tibbles <5703945+RealmOfTibbles@users.noreply.github.com>
2019-05-20 14:29:16 +0100

    Solid Color Settings

--- Base.py
+++ Base.py
@@ -8,13 +8,15 @@ import adafruit_dotstar as dotstar
 Animation = 1
 # 1 R B bank wave 
 # 2 Solid Color
-#
+# 2 Static Color PerBank
 #
 dots_len = 42
 dots_lum = 0.5
 
-
-
+Animation_2_Values = [ 200, 0, 100]
+#                       R   G   B
+Animation_3_Values = [ 200, 0, 100, 100, 0, 200, 200, 0, 100]
+#            Top      R   G   B    Middle R   G  B   Bottom    
 
 
 
@@ -58,10 +60,10 @@ bank_b_subBank3_Start = bank_B_start
 bank_b_subBank3_End =   27
 
 # HELPERS
-# a random color 0 -> 224
+# a random color 0 -> 240
 def random_color():
     return random.randrange(0, 60) * 4
-
+#           15-> 210
 def random_color_limited():
     return random.randrange(1, 14) * 15
 
@@ -118,7 +120,7 @@ if Animation == 1 :
 
         R = color_modifi(R, 20)
         B = color_modifi(B, 40)
-
+        # Bottom Bank
         for dot in range(bank_a_subBank3_Start,bank_a_subBank3_End):
             dots[dot] = (R , G, B, L)
         # Sub bank other side     
@@ -127,3 +129,31 @@ if Animation == 1 :
 
         dots.show()
         time.sleep(0.05)
+
+if Animation == 2 :
+    # Top row Left
+    for dot in range(bank_A_Start,bank_B_End):
+        dots[dot] = (Animation_2_Values[0], Animation_2_Values[1], Animation_2_Values[2])
+    dots.show()
+
+if Animation == 3 :
+    # Top row Left
+    for dot in range(bank_a_subBank1_Start,bank_a_subBank1_End):
+        dots[dot] = (Animation_3_Values[1] , Animation_3_Values[2], Animation_3_Values[2])
+    # Sub bank other side     
+    for dot in range(bank_b_subBank1_Start,bank_b_subBank1_End):
+        dots[dot] = (Animation_3_Values[1] , Animation_3_Values[2], Animation_3_Values[2])
+
+    for dot in range(bank_a_subBank2_Start,bank_a_subBank2_End):
+        dots[dot] = (Animation_3_Values[3] , Animation_3_Values[4], Animation_3_Values[5])
+        # Sub bank other side     
+    for dot in range(bank_b_subBank2_Start,bank_b_subBank2_End):
+        dots[dot] = (Animation_3_Values[3] , Animation_3_Values[4], Animation_3_Values[5])
+
+        # Bottom Bank
+    for dot in range(bank_a_subBank3_Start,bank_a_subBank3_End):
+        dots[dot] = (Animation_3_Values[6] , Animation_3_Values[7], Animation_3_Values[8])
+        # Sub bank other side     
+    for dot in range(bank_b_subBank3_Start,bank_b_subBank3_End):
+        dots[dot] = (Animation_3_Values[6] , Animation_3_Values[7], Animation_3_Values[8])
+    dots.show()
